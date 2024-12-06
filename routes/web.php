b45ac91419424c141849c059f04033788fc2667d
JesusChamiso <jesus.chamiso@hotmail.com>
2024-12-05 20:39:44 -0400

    Testing Http y listado

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'tags' => Tag::get()
+    ]);
 });
 
 Route::get('about', function () {
