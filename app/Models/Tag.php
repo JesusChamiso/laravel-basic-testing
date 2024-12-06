78d109fa9ed318f1e57e866025e1353638fec269
JesusChamiso <jesus.chamiso@hotmail.com>
2024-12-05 21:34:21 -0400

    Pruebas unitarias a cambios

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,4 +9,10 @@ class Tag extends Model {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
     protected $fillable = ['name'];
+
+    public function getSlugAttribute() {
+        return strtolower(
+            str_replace(' ', '-', $this->name)
+        );
+    }
 }
